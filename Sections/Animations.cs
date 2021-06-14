c72c66d00118c1dea3513655b6fa71408be7a473
KaizerHind <42819568+KaizerHind@users.noreply.github.com>
2021-06-14 18:54:34 -0500

    v1.2.1

--- Sections/Animations.cs
+++ Sections/Animations.cs
@@ -26,7 +26,7 @@ namespace VisualTexture_v2.Sections
             //Visualization solution in Cefsharp - by DarThinking
             CefSettings settings = new CefSettings();
             //Cef.Initialize(settings);
-            chromeBrowser = new ChromiumWebBrowser("https://i.imgur.com/JsnBMiM.png")
+            chromeBrowser = new ChromiumWebBrowser("http://www.thornhillbc.org.uk/wp/wp-content/uploads/2017/09/Refresh-Logo-1024x604.jpg")
             {
                 Dock = DockStyle.Fill,
             };
@@ -92,8 +92,17 @@ namespace VisualTexture_v2.Sections
             "-  WALK_fatold",
             "-  WALK_gang1",
             "-  WALK_gang2",
+            "-  WALK_old",
+            "-  WALK_player",
+            "-  WALK_rocket",
+            "-  WALK_shuffle",
             "-  Walk_Wuzi",
+            "-  WOMAN_walkbusy",
+            "-  WOMAN_walkfatold",
+            "-  WOMAN_walknorm",
+            "-  WOMAN_walkold",
             "-  WOMAN_walkpro",
+            "-  WOMAN_walksexy",
             "-  WOMAN_walkshop",
             "-  XPRESSscratch",
             "",
@@ -521,6 +530,358 @@ namespace VisualTexture_v2.Sections
                     richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
                     richTextBox2.Text = "ApplyActorAnimation(Variable, PED, pass_Smoke_in_car, 4.0, 1, 1, 1, 0, 0)";
                     break;
+
+                case 30:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/phone_in/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, phone_in, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 31:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/phone_out/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, phone_out, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                 case 32:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/phone_talk/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, phone_talk, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 33:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/roadcross/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, roadcross, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 34:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/run_1armed/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, run_1armed, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 35:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/run_armed/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, run_armed, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 36:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/run_civi/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, run_civi, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 37:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/run_fatold/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, run_fatold, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 38:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/run_gang1/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, run_gang1, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 39:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/run_old/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, run_old, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 40:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/run_player/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, run_player, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 41:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/Run_stop/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, Run_stop, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 42:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/Run_Wuzi/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, Run_Wuzi, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 43:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/SEAT_down/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, SEAT_down, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 44:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/SEAT_idle/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, SEAT_idle, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 45:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/SEAT_up/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, SEAT_up, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 46:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/Smoke_in_car/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, Smoke_in_car, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 47:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/Swim_Tread/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, Swim_Tread, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 48:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WALK_drunk/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WALK_drunk, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 49:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WALK_fatold/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WALK_fatold, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 50:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WALK_gang1/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WALK_gang1, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 51:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WALK_gang2/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WALK_gang2, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 52:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WALK_old/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WALK_old, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 53:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WALK_player/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WALK_player, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 54:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WALK_rocket/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WALK_rocket, 4.0, 1, 1, 1, 0, 0)";
+                    break;
+
+                case 55:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WALK_shuffle/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WALK_shuffle, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 56:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/Walk_Wuzi/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, Walk_Wuzi, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 57:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WOMAN_walkbusy/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WOMAN_walkbusy, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 58:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WOMAN_walkfatold/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WOMAN_walkfatold, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 59:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WOMAN_walknorm/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WOMAN_walknorm, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 60:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WOMAN_walkold/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WOMAN_walkold, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 61:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WOMAN_walkpro/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WOMAN_walkpro, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                case 62:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WOMAN_walksexy/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WOMAN_walksexy, 4.0, 1, 1, 1, 0, 0)";
+                    break;      
+
+                case 63:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/WOMAN_walkshop/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, WOMAN_walkshop, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+                 case 64:
+                    richTextBox1.Visible = true;
+                    richTextBox1.ReadOnly = true;
+                    panel3.Visible = true;
+
+                    chromeBrowser.Load("https://demo-anims.netlify.app/XPRESSscratch/");
+                    richTextBox1.Text = "new Variable Var = CreateActor(ID, X, Y, Z, RZ);";
+                    richTextBox2.Text = "ApplyActorAnimation(Variable, PED, XPRESSscratch, 4.0, 1, 1, 1, 0, 0)";
+                    break; 
+
+   
             }
         }
 
